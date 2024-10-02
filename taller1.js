30032404aeaa57c45d3c35dce04b60da4dad31b9
JuanOlaya <161900436+JuanXD2201@users.noreply.github.com>
2024-10-01 19:46:51 -0500

    Add files via upload

---
+++ taller1.js
@@ -0,0 +1,26 @@
+let a 
+a = 1
+document.write('Valor de a: ',a);
+document.write('<br>');
+document.write('<br>');
+a++;
+document.write('Valor de a: ',a);
+document.write('<br>');
+a+=5;
+document.write('Valor de a: ',a);
+document.write('<br>');
+a-=1;
+document.write('Valor de a: ',a);
+document.write('<br>');
+a-=2;
+document.write('Valor de a: ',a);
+document.write('<br>');
+a*=2;
+document.write('Valor de a: ',a);
+document.write('<br>');
+a/=2;
+document.write('Valor de a: ',a);
+document.write('<br>');
+a*=4;
+document.write('Valor de a: ',a);
+document.write('<br>');
